84d1ba7130e20620ea70e5946af2fe7858c3ec8b
MaheshFSD <maheshfsdeveloper@gmail.com>
2021-07-27 01:35:02 +0530

    Blog app started

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./TopBar.css"
+
+const TopBar = () => {
+    return (
+        <div className="top">
+            TopBar
+        </div>
+    )
+}
+
+export default TopBar
